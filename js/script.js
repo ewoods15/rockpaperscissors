b835ca0e04a56f557570f43243ada12aec906450
ewoods15 <ewoods.v@rrghst.org>
2019-11-13 23:20:43 +0000

    nov 13

--- js/script.js
+++ js/script.js
@@ -8,7 +8,33 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-$("button").click(function(){
-   let userChioice=  $("#shoot").val()
+$("#shoot").click(function(){
+   let userChoice =  $("#input").val()
    $("#userChoice").html(userChoice)
+   let randomVar = Math.random()
+   let x = computerChoice(randomVar)
+   $("#computerChoice").html(x)
+
+
+
+   if ( userChoice == x ){
+       $("").html()
+   }
 });
+
+
+function computerChoice(number){
+  if (number > 0.66){
+    return "rock"
+  }
+  if (number < 0.66 && number > 0.33 ){
+    return "paper"
+  }
+  else if (number < 0.33){
+    return "scissors"
+  }
+  
+}
+
+
+
